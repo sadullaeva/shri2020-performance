32ce426c395c51a87eaad68a91805945e214ab44
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-04-07 10:08:50 +0600

    Decreased initial scripting

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-    const buttonsContainer = document.querySelector(".buttons-wrap");
-    const fridgeInfoContainer = document.querySelector(".card_size_m:nth-child(8) .card-description");
     const confirmPurchaseButton = document.querySelector(".buttons-wrap .button_yellow");
-    const purchaseListContainer = document.createElement('div');
-    purchaseListContainer.innerHTML =
-        '<div class="purchase-list-wrap">' +
-        '<p class="card-description card-description_big description_critical">Список покупок:</p>' +
-        '<ol class="purchase-list">' +
-        '<li class="purchase-list__item">Хлеб</li>' +
-        '<li class="purchase-list__item">Молоко</li>' +
-        '</ol>' +
-        '</div>';
 
     confirmPurchaseButton.onclick = () => {
+        const buttonsContainer = document.querySelector(".buttons-wrap");
+        const fridgeInfoContainer = document.querySelector(".card_size_m:nth-child(8) .card-description");
+        const purchaseListContainer = document.createElement('div');
+        purchaseListContainer.innerHTML =
+            '<div class="purchase-list-wrap">' +
+            '<p class="card-description card-description_big description_critical">Список покупок:</p>' +
+            '<ol class="purchase-list">' +
+            '<li class="purchase-list__item">Хлеб</li>' +
+            '<li class="purchase-list__item">Молоко</li>' +
+            '</ol>' +
+            '</div>';
         fridgeInfoContainer.replaceWith(purchaseListContainer);
         buttonsContainer.style.display = "none";
     };
